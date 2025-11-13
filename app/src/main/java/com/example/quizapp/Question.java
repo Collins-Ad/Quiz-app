471d68742af93f28e2c1c72237f7fb9741d9b24a
mayowaadeniji9@gmail.com <mayowaadeniji9@gmail.com>
2025-11-13 10:59:06 -0500

    First rendition of quiz app

---
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -0,0 +1,16 @@
+package com.example.quizapp;
+
+import java.util.ArrayList;
+
+public class Question {
+    public Question(String q, String answer){
+
+ArrayList<String> questions = new ArrayList<Boolean>();
+        questions.add("Can Batman beat Superman?");
+        ArrayList<Boolean> answers = new ArrayList<Boolean>();
+        answers.add(true);
+        answers.add(false)
+
+
+    }
+}
