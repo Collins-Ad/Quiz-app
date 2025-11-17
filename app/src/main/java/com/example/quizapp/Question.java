96fa1e50057aca695cb8947df1d5e8c567100787
mayowaadeniji9@gmail.com <mayowaadeniji9@gmail.com>
2025-11-17 10:58:12 -0500

    First rendition of quiz app

--- app/src/main/java/com/example/quizapp/Question.java
+++ app/src/main/java/com/example/quizapp/Question.java
@@ -1,15 +1,28 @@
 package com.example.quizapp;
 
 import java.util.ArrayList;
-
+import java.util.List;
 public class Question {
-    public Question(String q, String answer){
+    List<String> questions = new ArrayList<>();
+    questions.add("Can Batman beat Superman?");
+    questions.add("Is Gotham the most dangerous city in the DC universe?");
+    questions.add("Is superman a government agent known as the atomic knight in an alternate universe?");
+    questions.add("Can ambush bug teleport through the 4th wall?");
+    questions.add("Was the character 'Condiment King' once part of the Justice league?");
 
-ArrayList<String> questions = new ArrayList<Boolean>();
-        questions.add("Can Batman beat Superman?");
-        ArrayList<Boolean> answers = new ArrayList<Boolean>();
-        answers.add(true);
-        answers.add(false)
+    ArrayList<String> answers = new ArrayList<String>();
+    answers.add("True");
+    answers.add("True");
+    answers.add("False");
+    answers.add("True");
+    answers.add("False");
+    public Question(String q, String answer){
+for (int i = 0; i < questions.size;i++){
+
+}
+
+
+
 
 
     }
